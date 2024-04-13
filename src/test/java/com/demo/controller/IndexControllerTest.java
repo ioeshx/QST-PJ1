7bb788542c807316757dcac540bdafba882a3560
ioeshx <228753522@qq.com>
2024-04-13 15:11:39 +0800

    index done

--- src/test/java/com/demo/controller/IndexControllerTest.java
+++ src/test/java/com/demo/controller/IndexControllerTest.java
@@ -1,6 +1,9 @@
 package com.demo.controller;
 
 import com.demo.entity.Message;
+import com.demo.entity.News;
+import com.demo.entity.Venue;
+import com.demo.entity.vo.MessageVo;
 import com.demo.service.MessageService;
 import com.demo.service.MessageVoService;
 import com.demo.service.NewsService;
@@ -16,6 +19,7 @@ import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -29,7 +33,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class IndexControllerTest {
     @Autowired
     private MockMvc mockMvc;
@@ -42,6 +45,43 @@ public class IndexControllerTest {
     private MessageVoService messageVoService;
     @MockBean
     private MessageService messageService;
+
+    private List<Message> mockMessageList(int size){
+        List<Message> messageList = new ArrayList<>();
+        for (int i = 0; i < size; i++) {
+            Message message = new Message(i, "user_test "+i, "test message "+i, LocalDateTime.now(), 2);
+            messageList.add(message);
+        }
+        return messageList;
+    }
+
+    private List<News> mockNewsList(int size){
+        List<News> newsList = new ArrayList<>();
+        for (int i = 0; i < size; i++) {
+            News news = new News(i, "title "+i, "content "+i, LocalDateTime.now());
+            newsList.add(news);
+        }
+        return newsList;
+    }
+
+    private List<Venue> mockVenueList(int size){
+        List<Venue> venueList = new ArrayList<>();
+        for (int i = 0; i < size; i++) {
+            Venue venue = new Venue(i, "venue "+i, "description "+i, i*100, "image "+i, "address "+i, "08:00", "22:00");
+            venueList.add(venue);
+        }
+        return venueList;
+    }
+
+    private List<MessageVo> mockMessageVoList(int size){
+        List<MessageVo> messageVoList = new ArrayList<>();
+        for (int i = 0; i < size; i++) {
+            MessageVo messageVo = new MessageVo(i, "user_test "+i, "content "+i, LocalDateTime.now(), "user_name_"+i, "image "+i, 2);
+            messageVoList.add(messageVo);
+        }
+        return messageVoList;
+    }
+
 
     /**
      * 测试访问index方法访问
@@ -75,29 +115,30 @@ public class IndexControllerTest {
      */
     @Test
     void indexTestWhenData() throws Exception {
+        int size = 3;
         when(newsService.findAll(any(Pageable.class)))
-                .thenReturn(new PageImpl<>(new ArrayList<>()) );
+                .thenReturn(new PageImpl<>(mockNewsList(size)) );
         when(venueService.findAll(any(Pageable.class)))
-                .thenReturn(new PageImpl<>(new ArrayList<>()) );
+                .thenReturn(new PageImpl<>(mockVenueList(size)) );
         when(messageService.findPassState(any(Pageable.class)))
-                .thenReturn(new PageImpl<>(new ArrayList<>()) );
+                .thenReturn(new PageImpl<>(mockMessageList(size)) );
         when(messageVoService.returnVo(any(List.class)))
-                .thenReturn(new ArrayList<>());
+                .thenReturn(mockMessageVoList(size));
+
 
         mockMvc.perform(get("/index"))
                 .andExpect(status().isOk())
                 .andExpect(view().name("index"))
                 .andExpect(model().attribute("user", nullValue()))
-                .andExpect(model().attribute("news_list", hasSize(0)))
-                .andExpect(model().attribute("venue_list", hasSize(0)))
-                .andExpect(model().attribute("message_list", hasSize(0)))
+                .andExpect(model().attribute("news_list", hasSize(size)))
+                .andExpect(model().attribute("venue_list", hasSize(size)))
+                .andExpect(model().attribute("message_list", hasSize(size)))
                 .andReturn();
 
         verify(newsService, times(1)).findAll(any(Pageable.class));
         verify(venueService, times(1)).findAll(any(Pageable.class));
         verify(messageService, times(1)).findPassState(any(Pageable.class));
         verify(messageVoService, times(1)).returnVo(any(List.class));
-
     }
 
     /**
--- src/test/java/com/demo/controller/user/UserControllerTest.java
+++ src/test/java/com/demo/controller/user/UserControllerTest.java
@@ -1,4 +1,45 @@
 package com.demo.controller.user;
 
+import com.demo.service.UserService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+@SpringBootTest
+@AutoConfigureMockMvc
 public class UserControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private UserService userService;
+
+
+    @Test
+    void signUpTest() throws Exception{
+        mockMvc.perform(get("/signup"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("signup"));
+    }
+
+    @Test
+    void loginTest() throws Exception{
+        mockMvc.perform(get("/login"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("login"));
+    }
+    @Test
+    void loginCheckTest() throws Exception{
+        mockMvc.perform(post("/loginCheck.do"))
+                .andExpect(status().isOk());
+    }
+
 }
