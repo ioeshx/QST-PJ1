1ed5b884a03087410c3eb6931fc54fbdd4b0dd95
ioeshx <228753522@qq.com>
2024-04-13 15:43:55 +0800

    userTest ongoing

--- src/test/java/com/demo/controller/user/UserControllerTest.java
+++ src/test/java/com/demo/controller/user/UserControllerTest.java
@@ -1,5 +1,6 @@
 package com.demo.controller.user;
 
+import com.demo.entity.User;
 import com.demo.service.UserService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -22,6 +26,16 @@ public class UserControllerTest {
     @MockBean
     private UserService userService;
 
+    private User  getMockUser(boolean isLegal, boolean isUser){
+        if(!isLegal)
+            return null;
+        User mockUser = mock(User.class);
+        if(isUser)
+            when(mockUser.getIsadmin()).thenReturn(0);
+        else
+            when(mockUser.getIsadmin()).thenReturn(1);
+        return mockUser;
+    }
 
     @Test
     void signUpTest() throws Exception{
@@ -37,9 +51,28 @@ public class UserControllerTest {
                 .andExpect(view().name("login"));
     }
     @Test
-    void loginCheckTest() throws Exception{
+    void loginCheckTestForUser() throws Exception{
+        when(userService.checkLogin(any(String.class), any(String.class)) )
+                .thenReturn(getMockUser(true,true));
         mockMvc.perform(post("/loginCheck.do"))
                 .andExpect(status().isOk());
     }
+
+    @Test
+    void loginCheckTestForAdmin() throws Exception{
+
+    }
+
+    @Test
+    void loginCheckForIllegalUser() throws Exception{
+
+    }
+
+    @Test
+    void loginCheckForIllegalAdmin() throws Exception{
+
+    }
+
+
 
 }
