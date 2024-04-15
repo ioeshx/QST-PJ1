5889c81a29282e4f38cc0c4d374090b2d3c9c070
ioeshx <228753522@qq.com>
2024-04-15 13:49:28 +0800

    AdminOrderControllerTest update

--- src/test/java/com/demo/controller/admin/AdminOrderControllerTest.java
+++ src/test/java/com/demo/controller/admin/AdminOrderControllerTest.java
@@ -1,4 +1,249 @@
 package com.demo.controller.admin;
 
+
+import com.demo.entity.Order;
+import com.demo.entity.vo.OrderVo;
+import com.demo.service.OrderService;
+import com.demo.service.OrderVoService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+
+@SpringBootTest
+@AutoConfigureMockMvc
 public class AdminOrderControllerTest {
+    @Autowired
+    MockMvc mockMvc;
+    @MockBean
+    private OrderService orderService;
+    @MockBean
+    private OrderVoService orderVoService;
+
+    private List<Order> getMockOrderList(int size){
+        List<Order> orderList = new ArrayList<>();
+        for (int i = 0; i < size; i++) {
+            Order order = new Order(i, "userID", i, 1, LocalDateTime.now(),
+                    LocalDateTime.now(), 1, 100);
+            orderList.add(order);
+        }
+        return orderList;
+    }
+    private List<OrderVo> getMockOrderVoList(List<Order> mockOrderList){
+        List<OrderVo> orderVoList = new ArrayList<>();
+        for (Order order : mockOrderList) {
+            OrderVo orderVo = new OrderVo(order.getOrderID(),
+                    order.getUserID(), order.getVenueID(),
+                    "venueName", order.getState(),
+                    order.getOrderTime(), order.getStartTime(),
+                    order.getHours(), order.getTotal());
+            orderVoList.add(orderVo);
+        }
+        return orderVoList;
+    }
+    // TODO reservation_manage 等价类划分应该有四种，还要补充
+    /**
+     * 当order数据存在时，测试reservation_manage方法
+     * @see AdminOrderController#reservation_manage
+     */
+    @Test
+    void reservation_manageTestWhenDataExists() throws Exception {
+        int size = 10;
+        List<Order> mockOrderList = getMockOrderList(size);
+        Page<Order> mockOrderPage = new PageImpl<>(mockOrderList);
+        List<OrderVo> mockOrderVoList = getMockOrderVoList(mockOrderList);
+        when(orderService.findAuditOrder())
+                .thenReturn(mockOrderList);
+        when(orderVoService.returnVo(any(List.class)))
+                .thenReturn(mockOrderVoList);
+        when(orderService.findNoAuditOrder(any(Pageable.class)))
+                .thenReturn(mockOrderPage);
+
+        mockMvc.perform(get("/reservation_manage"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("admin/reservation_manage"))
+                .andExpect(model().attribute("order_list", mockOrderVoList))
+                .andExpect(model().attribute("total", mockOrderPage.getTotalPages()));
+
+        verify(orderService,times(1)).findAuditOrder();
+        verify(orderVoService,times(1)).returnVo(any(List.class));
+        verify(orderService,times(1)).findNoAuditOrder(any(Pageable.class));
+    }
+
+    /**
+     * 当order数据不存在时，测试reservation_manage方法
+     * @see AdminOrderController#reservation_manage
+     */
+    @Test
+    void reservation_manageTestWhenNoData() throws Exception {
+        List<Order> mockOrderList = new ArrayList<>();
+        Page<Order> mockOrderPage = new PageImpl<>(mockOrderList);
+        List<OrderVo> mockOrderVoList = getMockOrderVoList(new ArrayList<>());
+        when(orderService.findAuditOrder())
+                .thenReturn(mockOrderList);
+        when(orderVoService.returnVo(any(List.class)))
+                .thenReturn(mockOrderVoList);
+        when(orderService.findNoAuditOrder(any(Pageable.class)))
+                .thenReturn(mockOrderPage);
+
+        mockMvc.perform(get("/reservation_manage"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("admin/reservation_manage"))
+                .andExpect(model().attribute("order_list", mockOrderVoList))
+                .andExpect(model().attribute("total", mockOrderPage.getTotalPages()));
+
+        verify(orderService,times(1)).findAuditOrder();
+        verify(orderVoService,times(1)).returnVo(any(List.class));
+        verify(orderService,times(1)).findNoAuditOrder(any(Pageable.class));
+    }
+    // TODO getNoAuditOrder 还要补充用例
+    /**
+     * 管理员查看未审核订单
+     * 当order数据存在并且参数Page正确时，测试getNoAuditOrder方法
+     * @see AdminOrderController#getNoAuditOrder
+     */
+    @Test
+    void getNoAuditOrderTestWhenDataExists() throws Exception {
+        int size = 10;
+        List<Order> mockOrderList = getMockOrderList(size);
+        Page<Order> mockOrderPage = new PageImpl<>(mockOrderList);
+        List<OrderVo> mockOrderVoList = getMockOrderVoList(mockOrderList);
+        when(orderService.findNoAuditOrder(any(Pageable.class)))
+                .thenReturn(mockOrderPage);
+        when(orderVoService.returnVo(any(List.class)))
+                .thenReturn(mockOrderVoList);
+
+        mockMvc.perform(get("/admin/getOrderList.do")
+                        .param("page", "1"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(size)));
+
+        verify(orderService,times(1)).findNoAuditOrder(any(Pageable.class));
+        verify(orderVoService,times(1)).returnVo(any(List.class));
+    }
+
+    /**
+     * 管理员查看未审核订单
+     * 当order数据不存在并且参数Page正确时，测试getNoAuditOrder方法
+     * @see AdminOrderController#getNoAuditOrder
+     */
+    @Test
+    void getNoAuditOrderTestWhenNoData() throws Exception {
+        List<Order> mockOrderList = new ArrayList<>();
+        Page<Order> mockOrderPage = new PageImpl<>(mockOrderList);
+        List<OrderVo> mockOrderVoList = getMockOrderVoList(mockOrderList);
+        when(orderService.findNoAuditOrder(any(Pageable.class)))
+                .thenReturn(mockOrderPage);
+        when(orderVoService.returnVo(any(List.class)))
+                .thenReturn(mockOrderVoList);
+
+        mockMvc.perform(get("/admin/getOrderList.do")
+                        .param("page", "1"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("[]"));
+
+        verify(orderService,times(1)).findNoAuditOrder(any(Pageable.class));
+        verify(orderVoService,times(1)).returnVo(any(List.class));
+    }
+
+    /**
+     * 管理员查看未审核订单
+     * 当参数Page错误(Page<=0)时，测试getNoAuditOrder方法
+     * 测试应该失败并抛出异常
+     * @see AdminOrderController#getNoAuditOrder
+     */
+    @Test
+    void getNoAuditOrderTestWhenPageArgumentWrong() throws Exception {
+        List<Order> mockOrderList = new ArrayList<>();
+        Page<Order> mockOrderPage = new PageImpl<>(mockOrderList);
+        List<OrderVo> mockOrderVoList = getMockOrderVoList(mockOrderList);
+        when(orderService.findNoAuditOrder(any(Pageable.class)))
+                .thenReturn(mockOrderPage);
+        when(orderVoService.returnVo(any(List.class)))
+                .thenReturn(mockOrderVoList);
+
+        mockMvc.perform(get("/admin/getOrderList.do")
+                        .param("page", "0"))    //Page参数小于或等于0会出错，是一个等价类
+                .andExpect(status().isOk())
+                .andExpect(content().json(mockOrderList.toString()));
+
+        verify(orderService,times(1)).findNoAuditOrder(any(Pageable.class));
+        verify(orderVoService,times(1)).returnVo(any(List.class));
+    }
+
+    /**
+     * 当orderId在数据库存在时，测试confirmOrder方法
+     * @see AdminOrderController#confirmOrder
+     */
+    @Test
+    void confirmOrderTestWhenIdExists() throws Exception {
+        doNothing().when(orderService).confirmOrder(anyInt());
+
+        mockMvc.perform(post("/passOrder.do")
+                        .param("orderID", "1"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("true"));
+    }
+
+    /**
+     * 当orderId在数据库不存在时，测试confirmOrder方法
+     * 这个测试应该抛出异常失败
+     * @see AdminOrderController#confirmOrder
+     */
+    @Test
+    void confirmOrderTestWhenIdNotExist() throws Exception {
+        doThrow(new IllegalArgumentException("Order ID Not Exists")).when(orderService).confirmOrder(anyInt());
+
+        mockMvc.perform(post("/passOrder.do")
+                        .param("orderID", "1"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("true"));
+    }
+
+    /**
+     * 当orderId在数据库存在时，测试rejectOrder方法
+     * @see AdminOrderController#rejectOrder
+     */
+    @Test
+    void rejectOrderTestWhenIDExists() throws Exception {
+        doNothing().when(orderService).rejectOrder(anyInt());
+
+        mockMvc.perform(post("/rejectOrder.do")
+                        .param("orderID", "1"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("true"));
+    }
+
+    /**
+     * 当orderId在数据库不存在时，测试rejectOrder方法
+     * 这个测试应该失败并抛出异常
+     * @see AdminOrderController#rejectOrder
+     */
+    @Test
+    void rejectOrderTestWhenIDNotExist() throws Exception {
+        doThrow(new IllegalArgumentException("Order ID Not Exists"))
+                .when(orderService).rejectOrder(anyInt());
+
+        mockMvc.perform(post("/rejectOrder.do")
+                        .param("orderID", "1"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("true"));
+    }
 }
